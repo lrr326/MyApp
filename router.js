baa44bd872996f709ef2422c27097818ba3c22f1
hzhyang <zilongerdai@gmail.com>
2019-07-20 18:18:28 +0800

    我的用户信息

--- router.js
+++ router.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation';
 import Login from './pages/Login';
+import Mine from './pages/Mine';
 
 
 const Router = createStackNavigator({
+        Mine: {
+            screen: Mine
+        }, // 我的
         Login: {
             screen: Login
         }, // 登录页
         
+        
     },
     {
-        initialRouteName: 'Login'
+        initialRouteName: 'Mine'
     });
 
 export default Router;
